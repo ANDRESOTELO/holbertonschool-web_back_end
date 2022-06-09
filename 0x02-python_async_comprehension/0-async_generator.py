43add16cf859f808317ef4d3e6fb3d058c3a3359
ANDRESOTELO <andresotelo2020@gmail.com>
2022-06-08 19:51:33 -0500

    ADDED | Asyncio

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+from asyncio import sleep
+from typing import Generator
+from random import uniform
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Async Generator
+    """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
