2b12abb2e5a927c709e7b63dc0db20fa3fa17352
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 19:26:52 -0500

    ADDED | add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -24,7 +24,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def __session(self) -> Session:
         """
         Memoized session object
         """
