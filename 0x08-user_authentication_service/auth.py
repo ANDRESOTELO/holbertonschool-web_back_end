b640b9743175d594db28f9c221c8f7b3c78c1fae
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 20:07:00 -0500

    ADDED | auth class

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -19,3 +19,29 @@ def _hash_password(password: str) -> str:
     Takes password as an argument and returns a hashed_password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register an user in the db
+        Return: User Object
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
+        else:
+            raise ValueError(f'User {email} already exists')
