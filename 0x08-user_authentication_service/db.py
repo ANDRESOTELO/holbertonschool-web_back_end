7abd088c26498baaff3c9212664d83d1f3c04cb2
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 19:49:51 -0500

    ADDED | add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,9 +5,6 @@ Database module for ORM
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.session import Session
 from user import User, Base
 
 
