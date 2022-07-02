40b2ade1a289b35b467f862eb06f71cc575c6dcc
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 19:25:14 -0500

    ADDED | add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -6,13 +6,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
 
 
 class DB:
     """
-    DataBase class
+    DataBase class for objetc relational mapping
     """
 
     def __init__(self) -> None:
@@ -36,7 +35,8 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Method to add user to the database and return user object
+        Method to add user to the database
+        Return user object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
