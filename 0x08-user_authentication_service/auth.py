9477eed7aeb7cf94a40f5da4f736689665bc835b
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 20:21:58 -0500

    ADDED | validate_user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -45,3 +45,17 @@ class Auth:
 
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Try to locating user by email and
+        checks that password matches hashed password if user
+        exists
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
