d71b8720d1120b253470573a6d0e71e0c600eb2a
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 20:13:29 -0500

    ADDED | uSERS METHOD

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
-
+AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
@@ -15,3 +16,21 @@ def welcome() -> str:
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user if it does not exist before"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
