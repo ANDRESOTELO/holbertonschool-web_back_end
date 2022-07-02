3301497f7f676f913e31ffdc52783f3d7cd0e01e
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-01 19:51:34 -0500

    ADDED | add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,6 +5,9 @@ Database module for ORM
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
 from user import User, Base
 
 
@@ -14,10 +17,8 @@ class DB:
     """
 
     def __init__(self) -> None:
-        """
-        Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
