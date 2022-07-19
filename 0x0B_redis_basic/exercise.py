4f346d5c2d87ae1e4321555ab92be3ff8797078e
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-19 18:30:01 -0500

    ADDED | Get method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -23,3 +23,35 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+
+    def get(self, key: str,
+        fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get method that take a key string and
+        convert to original type
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+
+    def get_str(self, key: str) -> str:
+        """
+        Automatically parametrize Cache.get to str
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+
+    def get_int(self, key: str) -> int:
+        """
+        Automatically parametrize Cache.get to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
