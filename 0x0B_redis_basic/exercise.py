8c72b44e3bef06c5161ce6c81e4a2f405c482e7f
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-19 17:46:14 -0500

    ADDED | Set variables

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 """
-
+Writing strings to Redis
 """
+import redis
+import uuid
+
+from typing import Union
+
+class Cache:
+    """Cache Class"""
+    def __init__(self):
+        """Constructor"""
+        self._redis = redis.Redis()
+
+
+    def store(self, data: Union[Union[Union[str, int], float], bytes]) -> str:
+        """
+        Store method
+        Returns -> Key
+        """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
