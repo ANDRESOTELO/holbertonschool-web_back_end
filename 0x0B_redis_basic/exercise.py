601f940af82a50edfdc848c95149e29d682bdabe
ANDRESOTELO <andresotelo2020@gmail.com>
2022-07-19 17:53:59 -0500

    ADDED | Set variables

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,6 +12,7 @@ class Cache:
     def __init__(self):
         """Constructor"""
         self._redis = redis.Redis()
+        self._redis.flushdb
 
 
     def store(self, data: Union[Union[Union[str, int], float], bytes]) -> str:
